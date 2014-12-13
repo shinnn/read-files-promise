e407ca9275b796f23bebad033d06dc2c0ebc74bc
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-12-13 18:31:28 +0900

    introduce wrap-promise
    https://github.com/shinnn/wrap-promise

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+/*!
+ * read-files-promise | MIT (c) Shinnosuke Watanabe
+ * https://github.com/shinnn/read-files-promise
+*/
 'use strict';
 
-var ES6Promise = global.Promise || require('es6-promise').Promise;
-
-var readFile = require('fs-readfile-promise');
+var readMultipleFiles = require('read-multiple-files');
+var wrapPromise = require('wrap-promise');
 
 module.exports = function readFilesPromise(filePaths, options) {
-  if (!Array.isArray(filePaths)) {
-    throw new TypeError(filePaths + ' is not an array.');
-  }
-
-  return ES6Promise.all(filePaths.map(function(filePath) {
-    return readFile(filePath, options);
-  }));
+  return wrapPromise(function(resolve, reject) {
+    readMultipleFiles(filePaths, options, function(err, bufs) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(bufs);
+    });
+  });
 };
